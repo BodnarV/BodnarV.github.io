96e720b7e8399263a83f92ae30cf0f500299be9c
VitaliyBodnar <bogdan_bodnar_01@ukr.net>
2018-11-06 18:47:01 +0200

    dgerg

--- new.js
+++
@@ -1,9 +0,0 @@
-function fun1(){
-var elem = document.getElementById("foto");
-    elem.style.margin = "500px";
-    
-  if(elem.style.margin=="500px"){
-      elem.style.margin="100px";
-  }
-}
-//addEventListener("click",fun1);
