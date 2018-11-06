1a4463f553fed012886c490827614a8f92882925
VitaliyBodnar <bogdan_bodnar_01@ukr.net>
2018-11-06 18:45:20 +0200

    656hj

---
+++ new.js
@@ -0,0 +1,9 @@
+function fun1(){
+var elem = document.getElementById("foto");
+    elem.style.margin = "500px";
+    
+  if(elem.style.margin=="500px"){
+      elem.style.margin="100px";
+  }
+}
+//addEventListener("click",fun1);
