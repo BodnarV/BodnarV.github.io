d561d47ed18487e31a94a37eb7849a4e7843e2c3
VitaliyBodnar <bogdan_bodnar_01@ukr.net>
2019-01-06 22:10:02 +0200

    ruuuuuuuuuuuuuuullll

---
+++ online/script.js
@@ -0,0 +1,13 @@
+
+ var c = document.querySelector(".b-menu-link");
+c.href = '../../index.html';
+
+var v = document.querySelector(".b-menu-link");
+v.href = '../../index.html';
+
+ var a = document.querySelector("iframe");
+c.width = '100%'; 
+c.height = '100%';
+
+// var b = document.querySelector("script");
+//b = '<script src="../script.js"></script>';
